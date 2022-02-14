db3e00aa86e83bc29a38d51d1a7eb143322993e5
Natalia Gomez Anton <marinat306@gmail.com>
2022-02-14 17:13:38 +0100

    cámara pruebas

---
+++ Unity/Umvra_v.0.1/Assets/scripts/Mov_Camara.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_Camara : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+
+    [SerializeField] float offsetY = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y + offsetY, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+    }
+}
